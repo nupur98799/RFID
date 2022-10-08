abf8ec6603561e4639aa0fddfabac7044ed79eb7
Nupur Ruchnadani <41247590+nupur98799@users.noreply.github.com>
2022-10-08 14:08:32 -0400

    Add files via upload

---
+++ RFID_1.ino
@@ -0,0 +1,41 @@
+
+int led=13;
+int count=0;
+void setup()
+{
+  Serial.begin(9600);//serial monitor
+ pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  if(Serial.available())
+  {
+digitalWrite(led,HIGH);
+delay(500);
+  {
+      
+      count=0; 
+      
+      {
+          char item=Serial.read();
+          Serial.print(item);
+          count++;
+          delay(5);
+      
+      }
+    
+        //Serial.println();
+       // Serial.print("Tag Length:  ");
+//       Serial.print(count);
+//        Serial.print("   Bytes  ");
+    
+
+  }
+  }
+else
+{
+  digitalWrite(led,LOW);
+}
+}
+  
---
+++ RFID_FINAL.ino
@@ -0,0 +1,56 @@
+char known_tag[12] = {"270012B370F6"};
+char input[12];
+int count = 0;
+unsigned char i;
+void setup()
+{
+  pinMode(2,OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(3,1);
+  if(Serial.available()>0)  
+  {
+    
+
+input[count] = Serial.read();
+count++;
+delay(5);
+if(count == 12)
+{
+count =0;
+Serial.print(input);
+
+for(i=6;i<12;i++)
+{
+if(known_tag[i] == input[i])
+  {
+
+  count=0;
+    Serial.println("ok"); 
+    digitalWrite(2,HIGH);
+digitalWrite(3,LOW);
+delay(1000);
+digitalWrite(2,LOW);
+}
+else
+{
+ digitalWrite(4,HIGH);
+delay(500);
+digitalWrite(4,LOW);
+}  
+}  
+  
+
+
+  }
+  }
+}
+
+  
+
+ 
